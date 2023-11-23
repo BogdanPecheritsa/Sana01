980626e0f4fcf917e3307c8944d602369a357444
BogdanPecheritsa <ipz231_pbd@student.ztu.edu.ua>
2023-11-23 17:36:57 +0200

    modified: LinearExpressions1/Program.cs

--- LinearExpressions1/Program.cs
+++ LinearExpressions1/Program.cs
@@ -1,4 +1,9 @@
-﻿double a, b, c, d, x, y, z, r;
+﻿using System.Text;
+
+double a, b, c, d, x, y, z, r;
+Console.OutputEncoding = Encoding.Unicode;
+Console.InputEncoding = Encoding.Unicode;
+
 Console.WriteLine("Задайте значення а:");
 a = double.Parse(Console.ReadLine());
 Console.WriteLine("Задайте значення b:");
@@ -14,7 +19,7 @@ Console.WriteLine($"x: {x}");
 y = 5 * (a + b) * (c - d) / 0.5 * c + d * d * ((a * a - b * b) / (b - a));
 Console.WriteLine($"y: {y}");
 
-z = (Math.Pow((x * x - 2 * x), 3) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b) / (5 * a + 3 * b);
+z = (Math.Pow(x * x - 2 * x, 3) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b) / (5 * a + 3 * b);
 Console.WriteLine($"z: {z}");
 
 r = (1.0 / 2 * a + 3.0 / 4 * b - 7.0 / 5) / (3 * c + 1) + 1 / (a - c);
